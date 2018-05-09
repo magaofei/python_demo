d4810b9e17fa154ce53534975bab00652faa62bf
magaofei <magaofei@100tal.com>
2018-05-09 14:58:26 +0800

    产出两个值的协程

--- generator/two_value.py
+++ generator/two_value.py
@@ -10,15 +10,14 @@ from inspect import getgeneratorstate
 """  协程未启动   """
 print(getgeneratorstate(my_coro2))
 
-s = simple_coro2(2)
 """  向前执行协程到第一个 yield 表达式(预激协程) 产出 a 的值,并且暂停"""
-next(s)
+next(my_coro2)
 
 """  GEN_SUSPENDED 协程在 yield 表达式处暂停  """
 print(getgeneratorstate(my_coro2))
 
 """  把数字28发给暂停的协程, 计算 yield 表达式,得到28, 然后把那个数字绑定给b, 打印, 然后协程暂停, 等待为 c 赋值  """
-s.send(28)
+my_coro2.send(28)
 
 """  把数字99发给暂停的协程, 计算 yield 表达式,得到99,然后把那个数绑定给 c ,打印, 然后协程终止, 生成器抛出 StopIteration 异常  """
-s.send(99)
+my_coro2.send(99)
