c237bcf977784b003a4b19dd6dcb37c51110ac4b
magaofei <magaofei@100tal.com>
2018-05-09 15:12:31 +0800

    完善 super

--- oop/super_method.py
+++ oop/super_method.py
@@ -1,5 +1,8 @@
 
-
+"""
+默认不调用父类的 __init__
+需要手动调用
+"""
 class Person(object):
     def __init__(self):
         print("Person init")
@@ -11,7 +14,25 @@ class Student(Person):
         super().__init__()
         print("Student init")
 
+class Child(Person):
+
+    def __init__(self):
+        super().__init__()
+        print("Child init")
+
+class H(Student, Child):
+    def __init__(self):
+        """
+        Person init
+        Child init
+        Student init
+        从最基类开始, 继承多个类时, 倒序初始化, 从参数右边开始
+        """
+        super().__init__()
+        print("H init")
 
 if __name__ == '__main__':
-    s = Student()
+    # s = Student()
+    h = H()
+
     
