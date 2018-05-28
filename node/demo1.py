0fcb7ff5dfc7ad006f336a420a7d4dfb3574b5de
Mark <mamian521@gmail.com>
2018-05-28 22:49:29 +0800

    add Node demo

---
+++ node/demo1.py
@@ -0,0 +1,6 @@
+class Node(object):
+
+    def __init__(self, value, left=None, right=None):
+        self.value = value  # The node value
+        self.left = left    # Left child
+        self.right = right  # Right child
